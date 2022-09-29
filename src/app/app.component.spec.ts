33538d1e83383ca14aeb1f56f75aa5a3f89f0b26
Gnoulelem Sibabi <gnoulelem@gmail.com>
2022-09-29 21:49:32 +0000

    removed a useless test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ll-30-sep-ng-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'll-30-sep-ng-app app is running!'
-    );
-  });
 });
